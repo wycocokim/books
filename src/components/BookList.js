d2900a3e1bca202e85421da13cde84f235099a82
Kim Kenet Wycoco <wycocokim@gmail.com>
2023-05-11 13:52:22 +0800

    first commit

---
+++ src/components/BookList.js
@@ -0,0 +1,14 @@
+import useBooksContext from "../hooks/use-books-context";
+import BookShow from "./BookShow";
+
+function BookList() {
+  const { books } = useBooksContext();
+
+  const renderedBooks = books.map((book) => {
+    return <BookShow key={book.id} book={book} />;
+  });
+
+  return <div className="book-list">{renderedBooks}</div>;
+}
+
+export default BookList;
